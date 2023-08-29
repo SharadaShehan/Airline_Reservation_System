49782c72b787d9b6868fb87ca807671d9f2e8525
Sharada Shehan <sharadashehan6@gmail.com>
2023-08-29 13:41:00 +0530

    file structure initialized

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Airline_Reservation_System
-DBMS group project for semester 3
+
+First Clone the repository into your local machine. 
+Go to location where you want to clone the project. Open a CLI and type below code.
+<pre>
+<code>git clone https://github.com/SharadaShehan/Airline_Reservation_System.git</code>
+</pre>
+
+
+
+
+
+
---
+++ backend/server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import datetime
+ 
+x = datetime.datetime.now()
+ 
+# Initializing flask app
+app = Flask(__name__)
+ 
+ 
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+ 
+    # Returning an api for showing in  reactjs
+    return {
+        'Name':"geek",
+        "Age":"22",
+        "Date":x,
+        "programming":"python"
+        }
+ 
+     
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
+
