103051126d2a49cd1639f67e3dea5ca49827ce55
Sharada Shehan <sharadashehan6@gmail.com>
2023-09-08 01:12:09 +0530

    tables created and populated with data

--- backend/app/scripts/create_tables.py
+++ backend/app/scripts/create_tables.py
@@ -9,12 +9,25 @@ def drop_all_tables():
         cursor = connection.cursor()
         drop_table_queries = []
         # Get all table names from the database
-        select_all_tables_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = %s;"
-        cursor.execute(select_all_tables_query, (current_app.config['MYSQL_DB'],))
+        tables_list = [
+            "Booking",
+            "Booking_Set",
+            "User",
+            "Category",
+            "Base_Price",
+            "Capacity",
+            "Class",
+            "Scheduled_Flight",
+            "Route",
+            "Location",
+            "Airport",
+            "Airplane",
+            "Model"
+        ]
         
         # Generate drop queries for all tables and append to drop_queries list
-        for table_name in cursor.fetchall():
-            drop_table_queries.append(f"DROP TABLE IF EXISTS {table_name[0]};")
+        for table_name in tables_list:
+            drop_table_queries.append(f"DROP TABLE IF EXISTS {table_name};")
         
         # Execute all drop queries to emptify the database
         for drop_query in drop_table_queries:
@@ -31,21 +44,170 @@ def create_tables():
     if connection:
         cursor = connection.cursor()
 
-        #------- Create users table -------
-        create_users_table_query = """
-            CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(50) NOT NULL,
-            email VARCHAR(100) NOT NULL );
+        #------- Create model table -------
+        create_model_table_query = """
+            CREATE TABLE IF NOT EXISTS Model (
+            Model_ID SMALLINT PRIMARY KEY AUTO_INCREMENT,
+            Name VARCHAR(40) NOT NULL );
         """
-        cursor.execute(create_users_table_query)
-        #-----------------------------------
+        cursor.execute(create_model_table_query)
+        #----------------------------------
 
-        #------- Create airports table -----
-        #-----------------------------------
+        #------- Create airplane table ----
+        create_airplane_table_query = """
+            CREATE TABLE IF NOT EXISTS Airplane (
+            Tail_Number VARCHAR(10) PRIMARY KEY,
+            Model SMALLINT NOT NULL,
+            FOREIGN KEY (Model) REFERENCES Model(Model_ID) );
+        """
+        cursor.execute(create_airplane_table_query)
+        #----------------------------------
+
+        #------- Create airport table -----
+        create_airport_table_query = """
+            CREATE TABLE IF NOT EXISTS Airport (
+            ICAO_Code CHAR(4) PRIMARY KEY,
+            IATA_Code CHAR(3) NOT NULL );
+        """
+        cursor.execute(create_airport_table_query)
+        #----------------------------------
+
+        #------- Create location table -----
+        create_location_table_query = """
+            CREATE TABLE IF NOT EXISTS Location (
+            Airport CHAR(4),
+            Level SMALLINT,
+            Name VARCHAR(20) NOT NULL,
+            PRIMARY KEY (Airport, Level),
+            FOREIGN KEY (Airport) REFERENCES Airport(ICAO_Code) );
+        """
+        cursor.execute(create_location_table_query)
+        #----------------------------------
+
+        #------- Create route table ------
+        create_route_table_query = """
+            CREATE TABLE IF NOT EXISTS Route (
+            Route_ID SMALLINT PRIMARY KEY AUTO_INCREMENT,
+            Origin CHAR(4) NOT NULL,
+            Destination CHAR(4) NOT NULL,
+            Duration_Minutes SMALLINT NOT NULL,
+            FOREIGN KEY (Origin) REFERENCES Airport(ICAO_Code),
+            FOREIGN KEY (Destination) REFERENCES Airport(ICAO_Code) );
+        """
+        cursor.execute(create_route_table_query)
+        #----------------------------------
+
+        #------- Create scheduled flight table ----
+        create_scheduled_flight_table_query = """
+            CREATE TABLE IF NOT EXISTS Scheduled_Flight (
+            Scheculed_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+            Route SMALLINT NOT NULL,
+            Airplane VARCHAR(10) NOT NULL,
+            Departure_Time DATETIME NOT NULL,
+            Delay_Minutes SMALLINT NOT NULL,
+            FOREIGN KEY (Route) REFERENCES Route(Route_ID),
+            FOREIGN KEY (Airplane) REFERENCES Airplane(Tail_Number) );
+        """
+        cursor.execute(create_scheduled_flight_table_query)
+        #----------------------------------
+
+        #------- Create class table -------
+        create_class_table_query = """
+            CREATE TABLE IF NOT EXISTS Class (
+            Class_Name VARCHAR(10) PRIMARY KEY,
+            Class_Code CHAR(1) NOT NULL );
+        """
+        cursor.execute(create_class_table_query)
+        #----------------------------------
+
+        #------- Create capacity table ----
+        create_capacity_table_query = """
+            CREATE TABLE IF NOT EXISTS Capacity (
+            Capacity_ID SMALLINT PRIMARY KEY AUTO_INCREMENT,
+            Model SMALLINT NOT NULL,
+            Class VARCHAR(10) NOT NULL,
+            Seats_Count SMALLINT NOT NULL,
+            FOREIGN KEY (Model) REFERENCES Model(Model_ID),
+            FOREIGN KEY (Class) REFERENCES Class(Class_Name) );
+        """
+        cursor.execute(create_capacity_table_query)
+        #----------------------------------
+
+        #------- Create base price table ----
+        create_base_price_table_query = """
+            CREATE TABLE IF NOT EXISTS Base_Price (
+            Price_ID SMALLINT PRIMARY KEY AUTO_INCREMENT,
+            Class VARCHAR(10) NOT NULL,
+            Route SMALLINT NOT NULL,
+            Price DECIMAL(8,2) NOT NULL,
+            FOREIGN KEY (Class) REFERENCES Class(Class_Name),
+            FOREIGN KEY (Route) REFERENCES Route(Route_ID) );
+        """
+        cursor.execute(create_base_price_table_query)
+        #----------------------------------
+
+        #------- Create category table ----
+        create_category_table_query = """
+            CREATE TABLE IF NOT EXISTS Category (
+            Category_ID SMALLINT PRIMARY KEY AUTO_INCREMENT,
+            Category_Name VARCHAR(10),
+            Min_Bookings SMALLINT NOT NULL, 
+            Discount DECIMAL(5,4) NOT NULL );
+        """
+        cursor.execute(create_category_table_query)
+        #----------------------------------
+
+        #------- Create user table ----
+        create_user_table_query = """
+            CREATE TABLE IF NOT EXISTS User (
+            Username VARCHAR(30) PRIMARY KEY,
+            Password VARCHAR(50) NOT NULL,
+            FirstName VARCHAR(30) NOT NULL,
+            LastName VARCHAR(30) NOT NULL,
+            IsAdmin BOOLEAN NOT NULL,
+            IsDataEntryOperator BOOLEAN NOT NULL,
+            Bookings_Count SMALLINT NOT NULL,
+            Category SMALLINT NOT NULL,
+            FOREIGN KEY (Category) REFERENCES Category(Category_ID) );
+        """
+        cursor.execute(create_user_table_query)
+        #----------------------------------
+
+        #------- Create booking set table ----
+        create_booking_set_table_query = """
+            CREATE TABLE IF NOT EXISTS Booking_Set (
+            Booking_Ref_ID CHAR(12) PRIMARY KEY ,
+            Scheduled_Flight INTEGER NOT NULL,
+            User VARCHAR(30),
+            BPrice_Per_Booking SMALLINT NOT NULL,
+            Final_Price DECIMAL(8,2) NOT NULL,
+            Completed BOOLEAN NOT NULL,
+            FOREIGN KEY (Scheduled_Flight) REFERENCES Scheduled_Flight(Scheculed_ID),
+            FOREIGN KEY (User) REFERENCES User(Username),
+            FOREIGN KEY (BPrice_Per_Booking) REFERENCES Base_Price(Price_ID) );
+        """
+        cursor.execute(create_booking_set_table_query)
+        #----------------------------------
+
+        #------- Create booking table ----
+        create_booking_table_query = """
+            CREATE TABLE IF NOT EXISTS Booking (
+            Ticket_Number INTEGER PRIMARY KEY AUTO_INCREMENT,
+            Booking_Set CHAR(12) NOT NULL,
+            Seat_Number SMALLINT NOT NULL,
+            FirstName VARCHAR(30) NOT NULL,
+            LastName VARCHAR(30) NOT NULL,
+            IsAdult BOOLEAN NOT NULL,
+            FOREIGN KEY (Booking_Set) REFERENCES Booking_Set(Booking_Ref_ID) );
+        """
+        cursor.execute(create_booking_table_query)
+        #----------------------------------
         
         connection.commit()
         connection.close()
     else:
         raise Exception("Failed to create tables")
+
+
+
 
--- backend/app/scripts/populate_data.py
+++ backend/app/scripts/populate_data.py
@@ -1,32 +1,281 @@
 from app.scripts.db import get_db_connection
 import csv
 
-# populate users table
-def populate_user_data():
+
+def populate_model_table():
     connection = get_db_connection()
     if connection:
         cursor = connection.cursor()
-        # Insert query for users table
-        insert_users_query = """INSERT INTO users (id, username, email) VALUES"""
-        # Read data from csv file and insert into users table
-        with open('app/scripts/data/users_data.csv', 'r') as file:       # use specific path to csv file
+        insert_model_query = """INSERT INTO Model (Name) VALUES"""
+        # Read data from csv file and insert into Model table
+        with open('app/scripts/data/Model.csv', 'r') as file:       # use specific path to csv file
             csv_reader = csv.reader(file)
             # Skip the header row
             header_row = next(csv_reader)
             for row in csv_reader:
                 # Get attributes for each record from comma separated row
-                id, username, email = row
-                insert_users_query += f"({id},{username},{email}),"
-            insert_users_query = insert_users_query[:-1] + ';'      # remove last comma and add semicolon
-        cursor.execute(insert_users_query)
+                name = row[0]
+                insert_model_query += f"({name}),"
+            insert_model_query = insert_model_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_model_query)
         connection.commit()
         connection.close()
     else:
-        raise Exception("Failed to populate users data")
+        raise Exception("Failed to populate model data")
 
+def populate_airplane_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_airplane_query = """INSERT INTO Airplane (Tail_Number, Model) VALUES"""
+        # Read data from csv file and insert into Airplane table
+        with open('app/scripts/data/Airplane.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                tail_number, model = row
+                insert_airplane_query += f"({tail_number}, {model}),"
+            insert_airplane_query = insert_airplane_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_airplane_query)
+        connection.commit()
+        connection.close()
+    else:
+        raise Exception("Failed to populate airplane data")
+    
+def populate_airport_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_airport_query = """INSERT INTO Airport (ICAO_Code, IATA_Code) VALUES"""
+        # Read data from csv file and insert into Airport table
+        with open('app/scripts/data/Airport.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                icao_code, iata_code = row
+                insert_airport_query += f"({icao_code}, {iata_code}),"
+            insert_airport_query = insert_airport_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_airport_query)
+        connection.commit()
+        connection.close()
+    else:
+        raise Exception("Failed to populate airport data")
 
+def populate_location_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_location_query = """INSERT INTO Location (Airport, Level, Name) VALUES"""
+        # Read data from csv file and insert into Location table
+        with open('app/scripts/data/Location.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                airport, level, name = row
+                insert_location_query += f"({airport}, {level}, {name}),"
+            insert_location_query = insert_location_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_location_query)
+        connection.commit()
+        connection.close()
+    else:
+        raise Exception("Failed to populate location data")
 
+def populate_route_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_route_query = """INSERT INTO Route (Origin, Destination, Duration_Minutes) VALUES"""
+        # Read data from csv file and insert into Route table
+        with open('app/scripts/data/Route.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                origin, destination, duration_minutes = row
+                insert_route_query += f"({origin}, {destination}, {duration_minutes}),"
+            insert_route_query = insert_route_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_route_query)
+        connection.commit()
+        connection.close()
+    else:
+        raise Exception("Failed to populate route data")
+
+def populate_scheduled_flight_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_scheduled_flight_query = """INSERT INTO Scheduled_Flight (Route, Airplane, Departure_Time, Delay_Minutes) VALUES"""
+        # Read data from csv file and insert into Scheduled_Flight table
+        with open('app/scripts/data/Scheduled_Flight.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                route, airplane, departure_time = row
+                insert_scheduled_flight_query += f"({route}, {airplane}, {departure_time}, 0),"
+            insert_scheduled_flight_query = insert_scheduled_flight_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_scheduled_flight_query)
+        connection.commit()
+        connection.close()
+    else:
+        raise Exception("Failed to populate scheduled flight data")
+
+def populate_class_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_class_query = """INSERT INTO Class (Class_Name, Class_Code) VALUES"""
+        # Read data from csv file and insert into Class table
+        with open('app/scripts/data/Class.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                class_name, class_code = row
+                insert_class_query += f"({class_name}, {class_code}),"
+            insert_class_query = insert_class_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_class_query)
+        connection.commit()
+        connection.close()
+
+def populate_capacity_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_capacity_query = """INSERT INTO Capacity (Model, Class, Seats_Count) VALUES"""
+        # Read data from csv file and insert into Capacity table
+        with open('app/scripts/data/Capacity.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                model, class_name, seats_count = row
+                insert_capacity_query += f"({model}, {class_name}, {seats_count}),"
+            insert_capacity_query = insert_capacity_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_capacity_query)
+        connection.commit()
+        connection.close()
+
+def populate_base_price_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_base_price_query = """INSERT INTO Base_Price (Class, Route, Price) VALUES"""
+        # Read data from csv file and insert into Base_Price table
+        with open('app/scripts/data/Base_Price.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                class_name, route, price = row
+                insert_base_price_query += f"({class_name}, {route}, {price}),"
+            insert_base_price_query = insert_base_price_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_base_price_query)
+        connection.commit()
+        connection.close()
+
+def populate_category_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_category_query = """INSERT INTO Category (Category_Name, Min_Bookings, Discount) VALUES"""
+        # Read data from csv file and insert into Category table
+        with open('app/scripts/data/Category.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                category_name, min_bookings, discount = row
+                insert_category_query += f"({category_name}, {min_bookings}, {discount}),"
+            insert_category_query = insert_category_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_category_query)
+        connection.commit()
+        connection.close()
+
+def populate_user_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_user_query = """INSERT INTO User (Username, Password, FirstName, LastName, IsAdmin, IsDataEntryOperator, Bookings_Count, Category) VALUES"""
+        # Read data from csv file and insert into User table
+        with open('app/scripts/data/User.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                username, password, first_name, last_name, is_admin, is_DEO, bookings_count, category = row
+                insert_user_query += f"({username}, {password}, {first_name}, {last_name}, {is_admin}, {is_DEO}, {bookings_count}, {category}),"
+            insert_user_query = insert_user_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_user_query)
+        connection.commit()
+        connection.close()
+
+def populate_booking_set_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_booking_set_query = """INSERT INTO Booking_Set (Booking_Ref_ID, Scheduled_Flight, User, BPrice_Per_Booking, Final_price, Completed) VALUES"""
+        # Read data from csv file and insert into Booking_Set table
+        with open('app/scripts/data/Booking_Set.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                booking_ref_id, scheduled_flight, user, bprice_per_booking, final_price, completed = row
+                if user == '':
+                    user = 'NULL'
+                insert_booking_set_query += f"({booking_ref_id}, {scheduled_flight}, {user}, {bprice_per_booking}, {final_price}, {completed}),"
+            insert_booking_set_query = insert_booking_set_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_booking_set_query)
+        connection.commit()
+        connection.close()
+
+def populate_booking_table():
+    connection = get_db_connection()
+    if connection:
+        cursor = connection.cursor()
+        insert_booking_query = """INSERT INTO Booking (Booking_Set, Seat_Number, FirstName, LastName, IsAdult) VALUES"""
+        # Read data from csv file and insert into Booking table
+        with open('app/scripts/data/Booking.csv', 'r') as file:       # use specific path to csv file
+            csv_reader = csv.reader(file)
+            # Skip the header row
+            header_row = next(csv_reader)
+            for row in csv_reader:
+                # Get attributes for each record from comma separated row
+                booking_set, seat_number, first_name, last_name, is_adult = row
+                insert_booking_query += f"({booking_set}, {seat_number}, {first_name}, {last_name}, {is_adult}),"
+            insert_booking_query = insert_booking_query[:-1] + ';'      # remove last comma and add semicolon
+        cursor.execute(insert_booking_query)
+        connection.commit()
+        connection.close()
 
 # Add more functions to populate data for other tables
 def populate_data():
-    populate_user_data()
+    populate_model_table()
+    populate_airplane_table()
+    populate_airport_table()
+    populate_location_table()
+    populate_route_table()
+    populate_scheduled_flight_table()
+    populate_class_table()
+    populate_capacity_table()
+    populate_base_price_table()
+    populate_category_table()
+    populate_user_table()
+    populate_booking_set_table()
+    populate_booking_table()
+
