50e65ebca175f7f8001885750bd24f1d72d42d42
Rusira de Silva <rusiradesilvaofc@gmail.com>
2023-09-07 23:20:33 +0530

    added loginForm, RegisterForm

---
+++ frontend/src/components/loginForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  doSubmit = () => {
+    // call the server
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", true)}
+          {this.renderInput("password", "Password", false, "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
---
+++ frontend/src/components/registerForm.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().email().required().label("Username"),
+    password: Joi.string().min(5).required().label("Password"),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = () => {
+    // call the server
+    console.log("Registered");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", true)}
+          {this.renderInput("password", "Password", false, "password")}
+          {this.renderInput("name", "Name", false)}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
