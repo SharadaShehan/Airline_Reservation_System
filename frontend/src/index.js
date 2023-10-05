747ef0f307cd94a943d2f0208c07d026fc6ea095
Rusira de Silva <rusiradesilvaofc@gmail.com>
2023-10-05 20:40:11 +0530

    Footer added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Footer from "./components/Layout/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
