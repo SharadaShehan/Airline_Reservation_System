89760d4ab99117e06350987ebb6793b348db3361
Rusira de Silva <rusiradesilvaofc@gmail.com>
2023-10-08 18:18:47 +0530

    Passengers_By_Flight admin profile fetch added

--- frontend/src/components/Profile/Admin/AdminProfile.jsx
+++ frontend/src/components/Profile/Admin/AdminProfile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UserMenuGlobalState } from "../../Layout/UserMenuGlobalState";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import AdminProfileDetails from "./AdminProfileDetails";
 import ViewRevenue from "./ViewRevenue";
 import PassengersByFlight from "./PassengersByFlight";
--- frontend/src/components/Profile/Admin/PassengersByFlight.jsx
+++ frontend/src/components/Profile/Admin/PassengersByFlight.jsx
@@ -1,90 +1,121 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./passengersByFlight.css";
 
 function PassengersByFlight({ setAdminMenuItem }) {
+  const [airportsList, setAirportsList] = useState([]);
+  const [origin, setOrigin] = useState("origin");
+  const [destination, setDestination] = useState("destination");
+  const [passengers, setPassengers] = useState([]);
+
+  useEffect(function () {
+    async function getAirportsList() {
+      try {
+        const response = await axios.get("http://34.93.16.70/api/get/airports");
+        console.log(response.data);
+        setAirportsList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAirportsList();
+  }, []);
+
   function handleBackClick() {
     setAdminMenuItem("profile-details");
   }
 
-  function handleViewClick() {
-    console.log("View Passenger By Flight");
+  async function handleViewClick() {
+    const token = "access_token";
+
+    console.log(origin, destination);
+    console.log(
+      `http://34.93.16.70/api/admin/next-flight/passengers?fromAirport=${origin}&toAirport=${destination}`
+    );
+
+    try {
+      const response = await axios.get(
+        `http://34.93.16.70/api/admin/next-flight/passengers?fromAirport=${origin}&toAirport=${destination}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data);
+      setPassengers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   }
   return (
     <div className="outer-box">
       <span className="view-revenue">View Passenger By Flight</span>
       <div className="selection-box">
-        <select className="model-selection" placeholder="Origin">
-          <option className="model-option" value="origin" selected>
+        <select
+          className="model-selection"
+          value={origin}
+          onChange={(e) => setOrigin(e.target.value)}
+        >
+          <option disabled value="origin" className="model-option">
             Origin
           </option>
-          <option className="model-option" value="model1">
-            Model 1
-          </option>
-          <option className="model-option" value="model2">
-            Model 2
-          </option>
-          <option className="model-option" value="model3">
-            Model 3
-          </option>
-          <option className="model-option" value="model4">
-            Model 4
-          </option>
+          {airportsList.map((airport) => (
+            <option
+              className="model-option"
+              value={airport.icaoCode}
+              key={airport.icaoCode}
+            >
+              {airport.city}
+            </option>
+          ))}
         </select>
-        <select className="model-selection" placeholder="Destination">
-          <option className="model-option" value="destination" selected>
+        <select
+          className="model-selection"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+        >
+          <option disabled value="destination" className="model-option">
             Destination
           </option>
-          <option className="model-option" value="model1">
-            Model 1
-          </option>
-          <option className="model-option" value="model2">
-            Model 2
-          </option>
-          <option className="model-option" value="model3">
-            Model 3
-          </option>
-          <option className="model-option" value="model4">
-            Model 4
-          </option>
+          {airportsList.map((airport) => (
+            <option
+              className="model-option"
+              value={airport.icaoCode}
+              key={airport.icaoCode}
+            >
+              {airport.city}
+            </option>
+          ))}
         </select>
       </div>
       <div className="inner-box">
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Passenger Name</th>
-              <th>Passenger Type</th>
-              <th>Seat Number</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Row 1, Column 1</td>
-              <td>Row 1, Column 2</td>
-              <td>Row 1, Column 3</td>
-              <td>Row 1, Column 4</td>
-            </tr>
-            <tr>
-              <td>Row 2, Column 1</td>
-              <td>Row 2, Column 2</td>
-              <td>Row 2, Column 3</td>
-              <td>Row 2, Column 4</td>
-            </tr>
-            <tr>
-              <td>Row 2, Column 1</td>
-              <td>Row 2, Column 2</td>
-              <td>Row 2, Column 3</td>
-              <td>Row 2, Column 4</td>
-            </tr>
-            <tr>
-              <td>Row 2, Column 1</td>
-              <td>Row 2, Column 2</td>
-              <td>Row 2, Column 3</td>
-              <td>Row 2, Column 4</td>
-            </tr>
-          </tbody>
-        </table>
+        {passengers.length === 0 ? (
+          <div className="no-passengers">
+            Select Origin and Destination airports
+          </div>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Passenger Name</th>
+                <th>Passenger Type</th>
+                <th>Seat Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {passengers.map((passenger) => (
+                <tr key={passenger.id}>
+                  <td>{passenger.date}</td>
+                  <td>{passenger.name}</td>
+                  <td>{passenger.userType}</td>
+                  <td>{passenger.seatNumber}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
       <div className="buttons-div">
         <button onClick={handleBackClick} className="buttons">
