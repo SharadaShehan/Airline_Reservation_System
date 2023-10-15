7635c6276451c74e62f6b1e6e79a3de0b620023c
Rusira de Silva <rusiradesilvaofc@gmail.com>
2023-10-15 23:17:06 +0530

    Booked Tickets page created

--- frontend/src/pages/BookedTickets.jsx
+++ frontend/src/pages/BookedTickets.jsx
@@ -1,27 +1,42 @@
 import { useEffect } from "react";
+import { UserGlobalState } from "../components/Layout/UserGlobalState";
 import { BookingStepGlobalState } from "../components/Layout/BookingStepGlobalState";
+import UserBookedTickets from "../components/BookedTickets/UserBookedTickets";
+import GuestLandingPage from "../components/BookedTickets/GuestLandingPage";
+import Default from "../components/BookedTickets/Default";
 import "../styles/bookedTickets.css";
+import Cookies from "js-cookie";
 
-
-export default function BookedTickets () {
+export default function BookedTickets() {
+  const { currentUserData } = UserGlobalState();
   const { setBookingStep } = BookingStepGlobalState();
+
+  const guestID = Cookies.get("guest-id");
+
   useEffect(() => {
     setBookingStep("flightSearch");
   }, [setBookingStep]);
-  
+
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <img
-        className='background-image'
-        alt='Rectangle'
-        src={require('../images/BookedTicketsPageBackImage.jpg')} />
-      <div className='bookedTicketsPage-container'>
-
-        {/* add content here */}
-
+        className="background-image"
+        alt="Rectangle"
+        src={require("../images/BookedTicketsPageBackImage.jpg")}
+      />
+      <div className="bookedTicketsPage-container">
+        {renderPage(currentUserData, guestID)}
       </div>
     </div>
   );
-};
+}
 
-
+function renderPage(userData, guestID) {
+  if (userData.username !== null && userData.role === undefined) {
+    return <UserBookedTickets userData={userData} />;
+  } else if (guestID) {
+    return <GuestLandingPage userData={userData} guestID={guestID} />;
+  } else {
+    return <Default />;
+  }
+}
