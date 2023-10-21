31383b6ef843b2d0fad0562cbf8b48f2926b2e7d
Rusira de Silva <rusiradesilvaofc@gmail.com>
2023-10-21 19:15:45 +0530

    Booked Tickets Date/Time Fixed

--- frontend/src/components/BookedTickets/Default.jsx
+++ frontend/src/components/BookedTickets/Default.jsx
@@ -29,58 +29,61 @@ function Default() {
 
   return (
     <div className="content">
-        <div className="search-bar-container">
-            <div className="search-bar">
-            <input
-              className="search"
-              type="search"
-              placeholder={"Search By Booking Ref ID"}
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-            </div>
+      <div className="search-bar-container">
+        <div className="search-bar">
+          <input
+            className="search"
+            type="search"
+            placeholder={"Search By Booking Ref ID"}
+            value={searchTerm}
+            onChange={handleSearch}
+          />
         </div>
-        <div className="booking-details-wrapper">
+      </div>
+      <div className="booking-details-wrapper">
         {bookedTickets.length ? (
           <div className="booking-details-container">
             <h1 className="booking-details">Booking Details</h1>
             <div className="scrollable-body">
-            <table className="user-table">
-              <thead className="user-thead">
-                <tr className="user-tr">
-                  <th className="user-th">Ticket Number</th>
-                  <th className="user-th">Passenger</th>
-                  <th className="user-th">Passport ID</th>
-                  <th className="user-th">From</th>
-                  <th className="user-th">To</th>
-                  <th className="user-th">Seat</th>
-                  <th className="user-th">Class</th>
-                  <th className="user-th">Depature Date</th>
-                  <th className="user-th">Depature Time</th>
-                </tr>
-              </thead>
-              <tbody className="user-tbody">
-                {bookedTickets.map((ticket) => (
-                  <tr className="user-tr" key={ticket.ticketNumber}>
-                    <td className="user-td">{ticket.ticketNumber}</td>
-                    <td className="user-td">{ticket.passenger}</td>
-                    <td className="user-td">{ticket.passportID}</td>
-                    <td className="user-td">{ticket.from.city}</td>
-                    <td className="user-td">{ticket.to.city}</td>
-                    <td className="user-td">{ticket.seat}</td>
-                    <td className="user-td">{ticket.class}</td>
-                    <td className="user-td">{ticket.departureDate}</td>
-                    <td className="user-td">{ticket.departureTime}</td>
+              <table className="user-table">
+                <thead className="user-thead">
+                  <tr className="user-tr">
+                    <th className="user-th">Ticket Number</th>
+                    <th className="user-th">Passenger</th>
+                    <th className="user-th">Passport ID</th>
+                    <th className="user-th">From</th>
+                    <th className="user-th">To</th>
+                    <th className="user-th">Seat</th>
+                    <th className="user-th">Class</th>
+                    <th className="user-th">Depature Date</th>
+                    <th className="user-th">Depature Time</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody className="user-tbody">
+                  {bookedTickets.map((ticket) => (
+                    <tr className="user-tr" key={ticket.ticketNumber}>
+                      <td className="user-td">{ticket.ticketNumber}</td>
+                      <td className="user-td">{ticket.passenger}</td>
+                      <td className="user-td">{ticket.passportID}</td>
+                      <td className="user-td">{ticket.from.city}</td>
+                      <td className="user-td">{ticket.to.city}</td>
+                      <td className="user-td">{ticket.seat}</td>
+                      <td className="user-td">{ticket.class}</td>
+                      <td className="user-td">
+                        {new Date(ticket.departureDate).toLocaleDateString()}
+                      </td>
+
+                      <td className="user-td">{ticket.departureTime}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         ) : (
-        <>
-        <h4 className="loading-text">Enter a Valid Booking Ref ID</h4>
-      </>
+          <>
+            <h4 className="loading-text">Enter a Valid Booking Ref ID</h4>
+          </>
         )}
       </div>
     </div>
