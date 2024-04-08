90a1755f51cfe7bcf1ce6c7416104423705efc12
Sharada Shehan <sharadashehan6@gmail.com>
2024-04-08 14:50:40 +0530

    modified to use redis cache

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,20 +2,19 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from app.config import Config
-from app.api.cache import cache
-from app.api.routes import api_bp
+from app.api.cache import AppCache
 from app.scripts.routes import api_bp_init
 from flask_jwt_extended import JWTManager
-
 
 app = Flask(__name__)
 CORS(app, origins="*", supports_credentials=True)
 
-cache.init_app(app)
+app.config.from_object(Config)
+AppCache.init_cache(app)
 
+from app.api.routes import api_bp
 app.register_blueprint(api_bp_init, url_prefix='/init')
 app.register_blueprint(api_bp, url_prefix='/api')
 
-app.config.from_object(Config)
 jwt = JWTManager(app)
 
