8a31d0990f4aaa4f4c6b7a31736caac9c73553c9
Sharada Shehan <sharadashehan6@gmail.com>
2024-04-08 14:49:11 +0530

    modified to use Singleton for cache object

--- backend/app/api/cache.py
+++ backend/app/api/cache.py
@@ -1,8 +1,19 @@
 from flask_caching import Cache
 
-configuration = {
-    'CACHE_TYPE': 'filesystem',
-    'CACHE_DIR': 'app/api/cache'
-}
 
-cache = Cache(config=configuration)
+class AppCache:
+    cache = None
+
+    @staticmethod
+    def init_cache(app):
+        configuration = {
+            'CACHE_TYPE': 'redis',
+            'CACHE_REDIS_HOST': app.config['REDIS_HOST'],
+            'CACHE_REDIS_PORT': app.config['REDIS_PORT'],
+        }
+        AppCache.cache = Cache(config=configuration)
+        AppCache.cache.init_app(app)
+
+    @staticmethod
+    def get_cache():
+        return AppCache.cache
